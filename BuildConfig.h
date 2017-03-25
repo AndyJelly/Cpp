5b0d1aaaf4a3c7c6c5a210316e5092aa2a7e8815
AndyJelly <AndyJelly@users.noreply.github.com>
2017-03-25 15:51:42 +0800

    Create BuildConfig.h
    针对不同平台，使用不同的编译器定义

---
+++ BuildConfig.h
@@ -0,0 +1,43 @@
+#ifndef __BASE_BUILDCONFIG_H__
+#define __BASE_BUILDCONFIG_H__
+
+// A set of macros to use for platform detection.
+#if defined(__APPLE__)
+#   define OS_MACOSX 1
+#elif defined(__linux__)
+#   define OS_LINUX 1
+#endif
+
+#if defined(_WIN32)
+#   define OS_WIN 1
+#elif defined(__FreeBSD__)
+#   define OS_FREEBSD 1
+#elif defined(__OpenBSD__)
+#   define OS_OPENBSD 1
+#elif defined(__sun)
+#   define OS_SOLARIS 1
+#else
+#   error Please add support for your platform in build/BuildConfig.h
+#endif
+
+#if defined(OS_LINUX) || defined(OS_FREEBSD) || defined(OS_OPENBSD) || defined(OS_SOLARIS)
+#   define USE_NSS 1  // Use NSS for crypto.
+#   define USE_X11 1  // Use X for graphics.
+#endif
+
+// For access to standard POSIXish features, use OS_POSIX instead of a
+// more specific macro.
+#if defined(OS_MACOSX) || defined(OS_LINUX) || defined(OS_FREEBSD) || defined(OS_OPENBSD) || defined(OS_SOLARIS)
+#   define OS_POSIX 1
+#endif
+
+// Compiler detection.
+#if defined(__GNUC__)
+#   define COMPILER_GCC 1
+#elif defined(_MSC_VER)
+#   define COMPILER_MSVC 1
+#else
+#   error Please add support for your compiler in build/BuildConfig.h
+#endif
+
+#endif // __BASE_BUILDCONFIG_H__
